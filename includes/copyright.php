11d837786ecc218910b25ff063ab3f1f2dfd9171
Leonardo Roldão <41723429+devleonardoroldao@users.noreply.github.com>
2026-06-16 11:17:28 -0300

    Update copyright.php

--- includes/copyright.php
+++ includes/copyright.php
@@ -1,11 +1,11 @@
-<footer class="mt-[30px] py-2 border-t border-gray-200">
+<footer class="mt-[30px] p-[30px] md:p-2 border-t border-gray-200">
   <div class="max-w-4xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center text-sm text-gray-500">
     
     <p>© 2026 thefrontend.org</p>
     
-    <div class="flex gap-6 mt-4 md:mt-0">
+      <div class="hidden md:flex gap-6 mt-4 md:mt-0">
       <span>v1.1.0</span>
-      <a href="https://thefrontend.org/license_and_download_policy" target="_blank" class="hover:text-black transition-colors underline underline-offset-4">
+      <a href="https://thefrontend.org/thefrontend-dashboard" target="_blank" class="hover:text-black transition-colors underline underline-offset-4">
         Changelog
       </a>
     </div>
